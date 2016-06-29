f8d517e20b49b00d5d99a6daf4ea3ec3e2582bb1
1996dylanriley <dylan.riley@nice.org.uk>
2016-06-29 10:55:23 +0100

    first commit

---
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SimpleBlog.Controllers;
+
+namespace SimpleBlog
+{
+    public class RouteConfig
+    {
+       
+        public static void RegisterRoutes(RouteCollection routes)
+
+        {
+            string[] namespaces = new[] { typeof(PostsController).Namespace };
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Login", "login", new { Controller = "Auth", Action = "login" }, namespaces);
+
+            routes.MapRoute("Home", "", new {Controller = "Posts", Action = "Index"}, namespaces );
+        }
+    }
+}
